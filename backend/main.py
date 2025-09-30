dabf08427535ef811a0969cde9881f8a72a0a475
pat-escano <akiirafilia@gmail.com>
2025-09-30 12:00:51 +0700

    added basic crud

--- backend/main.py
+++ backend/main.py
@@ -48,7 +48,8 @@ def update_contact(user_id):
     
     return jsonify({"message": "User updated."}), 200
 
-@app.route("/delete_contact/<int:user_id", methods=["DELETE"])
+#delete contact
+@app.route("/delete_contact/<int:user_id>", methods=["DELETE"])
 def delete_contact(user_id):
     contact = Contact.query.get(user_id)
 
